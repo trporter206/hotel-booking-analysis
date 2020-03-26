ca528c106b9c88577f7dc629edf30fdaf4342608
Torri Porter <torriporter@Torris-MacBook-Pro.local>
2020-03-26 13:47:42 -0700

    improved charts

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
 
 bookings = pd.read_csv('hotel_bookings.csv')
 df = bookings.copy()
@@ -68,31 +69,35 @@ country_data.drop(columns='country', inplace=True)
 fig = px.pie(country_data,
              values='Number of Guests',
              names=country_data.index,
-             title='home country',
-             template='seaborn')
+             title='Home Country of Guests by %',
+             template='seaborn',)
 fig.update_traces(textposition="inside", textinfo="value+percent+label")
 
 guest_map = px.choropleth(country_data,
                           locations = country_data.index,
                           color = country_data['% of guests'],
                           hover_name = country_data.index,
-                          color_continuous_scale=px.colors.sequential.Plasma,
+                          color_continuous_scale='Viridis',
                           title="Home country of guests")
 
 # fig.show()
 # guest_map.show()
 
-# how much do guests pay -------------------------------------------------------
+# how much do guests pay per night/person --------------------------------------
 
 df['adr_pp'] = df['adr'] / df['total_guests']
 df_guests = df.loc[df['is_canceled'] == 0]
 room_prices = df[['hotel', 'reserved_room_type', 'adr_pp']].sort_values('reserved_room_type')
 
-plt.figure(figsize=(6,4))
+plt.figure(figsize=(8,5))
+sns.set()
+sns.set_style(rc={'grid.color': '.8',
+                  'axes.facecolor': 'white',})
 sns.boxplot(x='reserved_room_type',
             y='adr_pp',
             hue='hotel',
             data=room_prices,
+            palette=['#7186a8', '#ffad14'],
             hue_order=['City Hotel', 'Resort Hotel'],
             fliersize=0)
 plt.title("Price of room types per night and person", fontsize=16)
@@ -102,24 +107,28 @@ plt.legend(loc="upper right")
 plt.ylim(0, 160)
 
 # how does price per night change over the year --------------------------------
-
 room_prices = df[['hotel', 'arrival_date_month', 'adr_pp']].sort_values('arrival_date_month')
-plt.figure(figsize=(6,4))
+months= ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+room_prices.replace(room_prices['arrival_date_month'].unique(), months, inplace=True)
+plt.figure(figsize=(8,5))
+sns.set_style(rc={'grid.color': '.8',
+                  'axes.facecolor': 'white',})
 sns.lineplot(x='arrival_date_month',
              y='adr_pp',
              hue='hotel',
              data=room_prices,
+             palette=['#7186a8', '#ffad14'],
              hue_order = ["City Hotel", "Resort Hotel"],
              ci="sd",
              size="hotel",
              sizes=(2.5, 2.5))
 plt.title("Room price per night and person over the year", fontsize=16)
-plt.xlabel("Month", fontsize=16)
+plt.xlabel('Months', fontsize=16)
 plt.xticks(rotation=45)
 plt.ylabel("Price [EUR]", fontsize=16)
-
-# what are the busiest months --------------------------------------------------
-
+#
+# # what are the busiest months --------------------------------------------------
+#
 city_guests = city_data.groupby('arrival_date_month')['hotel'].count()
 resort_guests = resort_data.groupby('arrival_date_month')['hotel'].count()
 
@@ -135,15 +144,25 @@ full_guests['month'].replace(months_num, months, inplace=True)
 full_guests.loc[(full_guests["month"] == "July") | (full_guests["month"] == "August"), "guests"] /= 3
 full_guests.loc[~((full_guests["month"] == "July") | (full_guests["month"] == "August")), "guests"] /= 2
 
-plt.figure(figsize=(6,4))
-sns.lineplot(x='month', y='guests', data=full_guests, hue='hotel', hue_order=["City Hotel", "Resort Hotel"], size="hotel", sizes=(2.5, 2.5), sort=False)
+plt.figure(figsize=(8,5))
+sns.set_style(rc={'grid.color': '.8',
+                  'axes.facecolor': 'white',})
+sns.lineplot(x='month',
+             y='guests',
+             data=full_guests,
+             hue='hotel',
+             palette=['#7186a8', '#ffad14'],
+             hue_order=["City Hotel", "Resort Hotel"],
+             size="hotel",
+             sizes=(2.5, 2.5),
+             sort=False)
 plt.title('Guests per Month', fontsize=16)
 plt.xlabel('Month', fontsize=16)
 plt.xticks(rotation=45)
 plt.ylabel('Total guests', fontsize=16)
-
-# how long do people stay ------------------------------------------------------
-
+#
+# # how long do people stay ------------------------------------------------------
+#
 city_nights = city_data['total_nights'].value_counts()
 resort_nights = resort_data['total_nights'].value_counts()
 
@@ -156,51 +175,50 @@ resort_nights_data = pd.DataFrame({'total nights': list(resort_nights.index),
 full_nights_data = pd.concat([resort_nights_data, city_nights_data], ignore_index=True)
 full_nights_data['count'] = (full_nights_data['count']/full_nights_data['count'].sum()) * 100
 
-plt.figure(figsize=(6,4))
+plt.figure(figsize=(8,5))
+sns.set_style(rc={'grid.color': '.8',
+                  'axes.facecolor': 'white',})
 sns.barplot(x='total nights',
             y='count',
             data=full_nights_data,
             hue='hotel',
+            palette=['#7186a8', '#ffad14'],
             hue_order=["City Hotel", "Resort Hotel"])
 plt.title("Length of stay", fontsize=16)
 plt.xlabel("Number of nights", fontsize=16)
 plt.ylabel("Guests [%]", fontsize=16)
 plt.legend(loc="upper right")
 plt.xlim(0,15)
-
-# bookings by market segment ---------------------------------------------------
-
+#
+# # bookings by market segment ---------------------------------------------------
+#
 city_segments = city_data['market_segment'].value_counts()
 resort_segments = resort_data['market_segment'].value_counts()
 
 city_segments_data = pd.DataFrame({'market segment': list(city_segments.index),
                                    'count': list(city_segments.values)})
 resort_segments_data = pd.DataFrame({'market segment': list(resort_segments.index),
-                                   'count': list(resort_segments.values)})
+                                     'count': list(resort_segments.values)})
 
-city_segment_fig = px.pie(city_segments_data,
-                          values=city_segments_data['count'],
-                          names=city_segments_data['market segment'],
-                          title='City bookings per market segment',
-                          template='seaborn')
-city_segment_fig.update_traces(rotation=-90, textinfo="percent+label")
+city_labels = city_segments_data['market segment']
+city_values = city_segments_data['count']
+resort_labels = resort_segments_data['market segment']
+resort_values = resort_segments_data['count']
 
-resort_segment_fig = px.pie(resort_segments_data,
-                          values=resort_segments_data['count'],
-                          names=resort_segments_data['market segment'],
-                          title='Resort bookings per market segment',
-                          template='seaborn')
-resort_segment_fig.update_traces(rotation=-90, textinfo="percent+label")
-
-# city_segment_fig.show()
-# resort_segment_fig.show()
-
-# ADR by market segment and room tpye ------------------------------------------
-plt.figure(figsize=(6,4))
+city_fig = go.Figure(data=[go.Pie(labels=city_labels, values=city_values)])
+resort_fig = go.Figure(data=[go.Pie(labels=resort_labels, values=resort_values)])
+# city_fig.show()
+# resort_fig.show()
+#
+# # ADR by market segment and room tpye ------------------------------------------
+plt.figure(figsize=(8,5))
+sns.set_style(rc={'grid.color': '.8',
+                  'axes.facecolor': 'white',})
 sns.barplot(x='market_segment',
             y='adr_pp',
             data=df,
             hue='reserved_room_type',
+            palette='bright',
             ci="sd",
             errwidth=1,
             capsize=0.1)
@@ -210,17 +228,17 @@ plt.xlabel("Market segment", fontsize=16)
 plt.xticks(rotation=45)
 plt.ylabel("ADR per person [EUR]", fontsize=16)
 plt.legend(loc="upper right")
-
-# look into airline data -------------------------------------------------------
-
+#
+# # look into airline data -------------------------------------------------------
+#
 aviation_data = df.loc[df['market_segment'] == 'Aviation']
 non_aviation_data = df.loc[df['market_segment'] != 'Aviation']
 
 # print aviation_data[['lead_time', 'adr_pp']].describe()
 # print non_aviation_data[['lead_time', 'adr_pp']].describe()
-
-# monthly cancellations --------------------------------------------------------
-
+#
+# # monthly cancellations --------------------------------------------------------
+#
 res_monthly_booking = df.loc[df['hotel'] == 'Resort Hotel']['arrival_date_month'].value_counts().sort_index()
 city_monthly_booking = df.loc[df['hotel'] == 'City Hotel']['arrival_date_month'].value_counts().sort_index()
 
@@ -236,21 +254,25 @@ city_cancel_data = pd.DataFrame({'month': list(city_monthly_booking.index),
                                  'cancellations': list(city_monthly_cancels.values),
                                  'hotel': 'City Hotel'})
 full_cancel_data = pd.concat([res_cancel_data, city_cancel_data], ignore_index=True)
+# full_cancel_data['month'].replace(months_num, months, inplace=True)
 
 full_cancel_data['cancellations'] = (full_cancel_data['cancellations']/full_cancel_data['bookings']) * 100
 
-plt.figure(figsize=(6,4))
-sns.barplot(x='month',
+plt.figure(figsize=(8,5))
+sns.set_style(rc={'grid.color': '.8',
+                  'axes.facecolor': 'white',})
+sns.lineplot(x='month',
             y='cancellations',
             data=full_cancel_data,
             hue='hotel',
+            palette=['#7186a8', '#ffad14'],
             hue_order = ["City Hotel", "Resort Hotel"])
 plt.title('Bookings and Cancellations per month')
 plt.ylabel('Cancellations [%]', fontsize=16)
 plt.xlabel('Month', fontsize=16)
-
-# cancel correlations ----------------------------------------------------------
-
+#
+# # cancel correlations ----------------------------------------------------------
+#
 cancel_corr = df.corr()['is_canceled']
 # print cancel_corr.abs().sort_values(ascending=False)[1:]
 # print df.groupby('is_canceled')['reservation_status'].value_counts()
@@ -267,5 +289,5 @@ cat_features = ["hotel","arrival_date_month","meal","market_segment",
 features = num_features+cat_features
 x=df.drop(['is_canceled'], axis=1)[features]
 y=df['is_canceled']
-print y
-# plt.show()
+
+plt.show()
